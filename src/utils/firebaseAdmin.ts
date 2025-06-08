da1c2be5fbc6df29912e544cc039e6b86c631813
Tuan-Cuong-Dev <buildinglocalbrand@gmail.com>
2025-06-08 11:06:47 +0700

    Initial commit

--- src/utils/firebaseAdmin.ts
+++ src/utils/firebaseAdmin.ts
@@ -1,15 +1,23 @@
 // src/utils/firebaseAdmin.ts
-
 import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
+  const projectId = process.env.FIREBASE_PROJECT_ID;
+  const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+
+  if (!projectId || !clientEmail || !privateKey) {
+    throw new Error('Missing Firebase admin credentials in environment variables.');
+  }
+
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      projectId,
+      clientEmail,
+      privateKey,
     }),
   });
 }
 
 export default admin;
+
