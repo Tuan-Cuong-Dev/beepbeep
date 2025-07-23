6f4a59a84278201607aa0f33b5a7aece68c9289f
Tuan-Cuong-Dev <buildinglocalbrand@gmail.com>
2025-07-23 16:22:08 +0700

    Update langugage

--- src/components/profile/MyBusinessSection.tsx
+++ src/components/profile/MyBusinessSection.tsx
@@ -10,7 +10,16 @@ export default function MyBusinessSection() {
 
   return (
     <div className="space-y-10">
-      {/* SECTION 1: Services provided by the user */}
+      
+      {/* SECTION 1: Organization or business entity the user is part of */}
+      <section>
+        <h2 className="text-lg sm:text-xl font-semibold mb-4 text-gray-800">
+          {t('my_business_section.organization_title')}
+        </h2>
+        <MyOrganizationInfo />
+      </section>
+
+      {/* SECTION 2: Services provided by the user */}
       <section>
         <h2 className="text-lg sm:text-xl font-semibold mb-4 text-gray-800">
           {t('my_business_section.services_title')}
@@ -18,13 +27,6 @@ export default function MyBusinessSection() {
         <MyServiceList />
       </section>
 
-      {/* SECTION 2: Organization or business entity the user is part of */}
-      <section>
-        <h2 className="text-lg sm:text-xl font-semibold mb-4 text-gray-800">
-          {t('my_business_section.organization_title')}
-        </h2>
-        <MyOrganizationInfo />
-      </section>
     </div>
   );
 }
